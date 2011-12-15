80d8d501352e3a53c60de5e1534e0d88b8fbe7b8
box2 <tba>
2011-12-14 22:40:26 -0500

    iptables rules documentation

--- unreg.lua
+++ unreg.lua
@@ -1,6 +1,22 @@
 --[[
+unreg.lua
+by box2 - 2011-12-10
+
 Do not allow unregistered users to participate in the hub
 Turn this on in times of proxy abusing romanains
+
+NOTE:  These .ro clowns usually are accompanied by DDoS attacks
+so this only stops them from gloating in hub.
+
+To reduce DDoS effectiveness by 70% or more, add these iptables rules:
+
+# DROP ip if more than 3 connection attempts within 100 seconds
+SECONDS=100
+BLOCKCOUNT=3
+
+iptables -A INPUT -p tcp --dport 411 -i eth0 -m state --state NEW -m recent --set --name 411
+iptables -A INPUT -p tcp --dport 411 -i eth0 -m state --state NEW -m recent --update --seconds ${SECONDS} --hitcount ${BLOCKCOUNT} --name 411 -j DROP
+
 ]]
 
 sOCName = SetMan.GetString( 24 ) or "SetMe"; --Bots not enabled in GUI? Change the value after the or.
